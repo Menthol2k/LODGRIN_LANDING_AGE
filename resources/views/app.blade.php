36a2c522596905642189b57e2b256f030df035ef
Octavian <octavian@twm.ro>
2024-10-11 22:05:15 +0300

    SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,6 +43,76 @@
     <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="3c2a88c0-0041-4097-b5ad-1ca0c4a1f9e5"
         data-blockingmode="auto" type="text/javascript"></script>
 
+    <!-- SEO -->
+    <script type="application/ld+json">
+        {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "LODGRIN",
+        "mainEntity": {
+                "@type": "ItemList",
+                "itemListElement": [
+                {
+                    "@type": "WebPageElement",
+                    "name": "Serviciile Noastre",
+                    "url": "https://www.lodgrin.ro/#services"
+                },
+                {
+                    "@type": "WebPageElement",
+                    "name": "Despre Noi",
+                    "url": "https://www.lodgrin.ro/#about"
+                },
+                {
+                    "@type": "WebPageElement",
+                    "name": "Cere oferta",
+                    "url": "https://www.lodgrin.ro/tariff"
+                },
+                {
+                    "@type": "WebPageElement",
+                    "name": "Contact",
+                    "url": "https://www.lodgrin.ro/#contact"
+                },
+                {
+                    "@type": "WebPageElement",
+                    "name": "Testimoniale",
+                    "url": "https://www.lodgrin.ro/#testimonials"
+                },
+                ]
+            }
+        }
+    </script>
+
+    <title>Lodgrin - Dezvoltare software</title>
+    <meta name="description"
+        content="Fie că ai nevoie de o platformă customizată sau de o aplicație scalabilă, suntem aici să transformăm viziunea ta în realitate. Cu soluții eficiente și design modern, îți oferim exact ceea ce ai nevoie pentru a reuși.">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="LODGRIN SRL">
+    <link rel="canonical" href="https://lodgrin.com/">
+    <meta property="og:title" content="Lodgrin - Dezvoltare software">
+    <meta property="og:description"
+        content="Fie că ai nevoie de o platformă customizată sau de o aplicație scalabilă, suntem aici să transformăm viziunea ta în realitate.">
+    <meta property="og:url" content="https://lodgrin.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Lodgrin">
+    <meta property="og:locale" content="ro_RO">
+    <meta property="og:image" content="https://lodgrin.com/images/lodgrin_logo_dark_background.png">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Lodgrin - Dezvoltare software">
+    <meta name="twitter:description"
+        content="Fie că ai nevoie de o platformă customizată sau de o aplicație scalabilă, suntem aici să transformăm viziunea ta în realitate.">
+    <meta name="twitter:image" content="https://lodgrin.com/images/lodgrin_logo_dark_background.png">
+    <link rel="icon" href="https://lodgrin.com/favicon.ico">
+
+    <!-- FAVICON -->
+
+    <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Lodgrin" />
+    <link rel="manifest" href="/site.webmanifest" />
+
     <!-- Scripts -->
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
